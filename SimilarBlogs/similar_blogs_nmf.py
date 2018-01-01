0a33b0fa271cfce058283259f65245e7c40a2125
thunderock <findashutoshtiwari@gmail.com>
2018-01-02 00:07:20 +0530

    adding lemmatizer and NMF support for same problem

--- SimilarBlogs/similar_blogs_nmf.py
+++ SimilarBlogs/similar_blogs_nmf.py
@@ -5,8 +5,28 @@ from nltk.corpus import names
 from nltk.stem import WordNetLemmatizer
 import cPickle as pkl
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import NMF
+
+def letters_only(astr):
+    return astr.isalpha()
+
+raw_posts = pkl.load(open("post_dataset.pkl", "rb"))
+posts = []
+
+names = set(names.words())
+cv = CountVectorizer(stop_words="english", max_features=500)
+lemmatizer = WordNetLemmatizer()
+
+for post in raw_posts:
+    posts.append(" ".join([lemmatizer.lemmatize(word.lower()) for word in post.split() if letters_only(word) and word not in names]))
+
+
+transformed = cv.fit_transform(posts)
+nmf = NMF(n_components=100, )
+
+
+
 
 
 
